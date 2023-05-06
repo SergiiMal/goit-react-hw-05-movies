18844884a207f18c1707a3793d7b42d2fa2089f3
SergiiMal <ser4ester@gmail.com>
2023-05-06 13:55:58 +0300

    +

---
+++ src/utilities/variables/css.js
@@ -0,0 +1,10 @@
+export const mainTextColor = '#161616';
+export const secondTextColor = '#fafafa';
+export const accentTextColor = '#9e6e02';
+
+export const mainBgColor = '#fafafa';
+export const secondBgColor = '#161616';
+export const accentBgColor = '#9e6e02';
+
+export const mainHoverEfect = '250ms linear';
+export const mainGradient = 'linear-gradient(to right, #9e6e02, #1f1c18)';
